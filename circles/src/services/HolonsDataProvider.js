24c505fbd64f16ef43819cf4a53651b1fe30703c
Patrik Opacic <opacic@gmail.com>
2023-04-02 08:54:42 +0200

    Holons

--- circles/src/services/HolonsDataProvider.js
+++ circles/src/services/HolonsDataProvider.js
@@ -6,10 +6,24 @@ export default class HolonsDataProvider extends DataProvider {
     async getCircle(circleId) {
         let response = await axios.get(circleId);
         if (response.status === 200) {
-            const circle = response.data;
-            if (circle) {
-                return circle;
-            }
+            const holon = response.data;
+            if (!holon) return null;
+
+            let circle = {};
+            circle.id = holon.id;
+            circle.name = holon.name;
+            circle.description = holon.description;
+            circle.picture = holon.image;
+            circle.author = holon.author;
+            circle.repository = holon.repository;
+            circle.homepage = holon.homepage;
+            circle.base = holon.geolocation ? { latitude: holon.geolocation.lat, longitude: holon.geolocation.lng } : null;
+            circle.tags = holon.keywords
+                ? holon.keywords.map((x) => {
+                      return { name: x, text: "#" + x };
+                  })
+                : [];
+            return circle;
         }
         return null;
     }
